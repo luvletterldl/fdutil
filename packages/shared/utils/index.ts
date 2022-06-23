367c2b264cba0e3156849a00b58f53c6a4627994
ldl <2510952840@qq.com>
2022-06-23 19:46:49 +0800

    feat: add isEmptyObject function

--- packages/shared/utils/index.ts
+++ packages/shared/utils/index.ts
@@ -1,3 +1,12 @@
 export function isDeveloping() {
   return location.host.includes(':')
 }
+
+/** 判断对象是否是空对象 */
+export function isEmptyObject(obj: Object) {
+  // because Object.keys(new Date()).length === 0;
+  // we have to do some additional check
+  return obj // 👈 null and undefined check
+  && Object.keys(obj).length === 0
+  && Object.getPrototypeOf(obj) === Object.prototype
+}
