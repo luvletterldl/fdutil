7b54b9852ae9743d7ce2ad53976f547072bbe555
ldl <2273220457@qq.com>
2022-08-26 10:31:58 +0000

    update: component placeholder-img

--- packages/core/usePlaceholderImg/component.ts
+++ packages/core/usePlaceholderImg/component.ts
@@ -1,32 +1,35 @@
 import { throwError } from '@fdutil/shared'
-import { defineComponent, h } from 'vue-demi'
+import { computed, defineComponent, h } from 'vue-demi'
 import type { UsePlaceholderImgOptions } from '.'
 
 export const PlaceholderImg = defineComponent<UsePlaceholderImgOptions>({
   name: 'PlaceholderImg',
   props: ['w', 'h', 'wh', 'text', 'bghex', 'texthex'] as unknown as undefined,
   setup(props) {
-    let url = 'https://via.placeholder.com/'
-    if (props.wh)
-      url += `${props.wh}`
-    else if (props.w && props.h)
-      url += `${props.w}x${props.h}`
-    else
-      throwError('PlaceholderImg: ', '[w, h] or [wh], must have one.')
-
-    if (props.bghex) {
-      url += url.endsWith('/') ? props.bghex : `/${props.bghex}`
-      if (!props.texthex)
-        url += '/333333'
-    }
-    if (props.texthex) {
-      if (!props.bghex)
-        url += url.endsWith('/') ? 'CCCCCC' : '/CCCCCC'
+    const url = computed(() => {
+      let u = 'https://via.placeholder.com/'
+      if (props.w && props.h)
+        u += `${props.w}x${props.h}`
+      else if (props.wh)
+        u += `${props.wh}`
       else
-        url += `/${props.texthex}`
-    }
-    if (props.text)
-      url += `?text=${props.text.replace(/\s+/g, '+')}`
+        throwError('PlaceholderImg: ', '[w, h] or [wh], must have one.')
+
+      if (props.bghex) {
+        u += u.endsWith('/') ? props.bghex : `/${props.bghex}`
+        if (!props.texthex)
+          u += '/333333'
+      }
+      if (props.texthex) {
+        if (!props.bghex)
+          u += url.value.endsWith('/') ? 'CCCCCC' : '/CCCCCC'
+        else
+          u += `/${props.texthex}`
+      }
+      if (props.text)
+        u += `?text=${props.text.replace(/\s+/g, '+')}`
+      return u
+    })
 
     return () => {
       return h('img', { src: url })
